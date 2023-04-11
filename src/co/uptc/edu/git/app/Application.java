a954ef170e9da7d87b0522cf96bcce2fb503af40
Ronaldmolinares <ronald.molinares01@uptc.edu.co>
2023-04-11 11:10:57 -0500

    Actividad solucionada

---
+++ src/co/uptc/edu/git/app/Application.java
@@ -0,0 +1,16 @@
+package co.uptc.edu.git.app;
+
+public class Application {
+    private static void emeneses() {
+        System.out.println("Hello Edgar Meneses");
+    }
+
+    private static void rMolinares(){
+        System.out.println("Buenos días");
+    }
+
+    public static void main(String[] args) {
+        emeneses();
+        rMolinares();
+    }
+}
