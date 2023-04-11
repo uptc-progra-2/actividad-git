c390a67c3c316133801b096b69a720685a3934d7
WilliamC111 <110690003+WilliamC111@users.noreply.github.com>
2023-04-11 11:08:44 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,14 @@
+package co.uptc.edu.git.app;
+
+public class Application {
+    private static void emeneses() {
+        System.out.println("Hello Edgar Meneses");
+    }
+    private static void williamC() {
+        System.out.println("Bom dia WilliamCely");
+    }
+
+    public static void main(String[] args) {
+        emeneses();
+    }
+}
