fa2054a963e6193ea3d0ff9273cc3ad7ced2ef50
FabianMoreno04 <nelson.moreno03@uptc.edu.co>
2023-04-11 11:25:33 -0500

    agrego metodo Numeros...:.

--- src/co/uptc/edu/git/app/Application.java
+++ src/co/uptc/edu/git/app/Application.java
@@ -4,8 +4,18 @@ public class Application {
     private static void emeneses() {
         System.out.println("Hello Edgar Meneses");
     }
-
+    
+    public static void conversionNumFabian (int number){
+		  String result = "";
+		  int thousands = number/1;
+		  int hundreds = number /10;
+		  int tens = number/100;
+	      int units = number/1000;
+		  result += "\n milesimas \n" + thousands + "\n centenas \n" + hundreds + "\n decenas \n" + tens + "\n unidades \n" + units; 
+		  System.out.println(result); 
+	  }
     public static void main(String[] args) {
         emeneses();
+        conversionNumFabian(5);
     }
 }
