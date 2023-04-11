20db790a51af856a1cdcd94dfeebd5bd24502bb7
edgarmenesesc7@gmail.com <edgarmenesesc7@gmail.com>
2023-04-11 10:58:45 -0500

    feat: add emeneses function

---
+++ src/co/uptc/edu/git/app/Application.java
@@ -0,0 +1,11 @@
+package co.uptc.edu.git.app;
+
+public class Application {
+    private static void emeneses() {
+        System.out.println("Hello Edgar Meneses");
+    }
+
+    public static void main(String[] args) {
+        emeneses();
+    }
+}
