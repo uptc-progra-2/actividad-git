f77171b7e5997654eb2c6be114e30ede68daa51e
javierlink22@gmail.com <javierlink22@gmail.com>
2023-04-11 11:07:44 -0500

    JavierLopez

--- src/co/uptc/edu/git/app/Application.java
+++ src/co/uptc/edu/git/app/Application.java
@@ -5,7 +5,12 @@ public class Application {
         System.out.println("Hello Edgar Meneses");
     }
 
+    public static void javierLopez(){
+        System.out.println("Hello Javier Lopez");
+    }
+
     public static void main(String[] args) {
         emeneses();
+        javierLopez();
     }
 }
