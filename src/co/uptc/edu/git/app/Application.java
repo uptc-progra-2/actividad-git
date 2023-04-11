4a76390a05affd9d0e7532c8d1d18178e7ecdaf4
Andres Maldonado <andres.maldonado02@uptc.edu.co>
2023-04-11 11:07:04 -0500

    Andres_Maldonado

---
+++ src/co/uptc/edu/git/app/Application.java
@@ -0,0 +1,16 @@
+package co.uptc.edu.git.app;
+
+public class Application {
+    private static void emeneses() {
+        System.out.println("Hello Edgar Meneses");
+    }
+
+    private static void aMaldonado() {
+        System.out.println("Hello Andres");
+    }
+
+    public static void main(String[] args) {
+        emeneses();
+        aMaldonado();
+    }
+}
