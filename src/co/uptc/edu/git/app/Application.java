3a40d8ee03617e978a877629f3028d33f1f37659
andres <andresbarrera427@gmail.com>
2023-04-11 11:40:09 -0500

    andres barrera3

---
+++ src/co/uptc/edu/git/app/Application.java
@@ -0,0 +1,17 @@
+package co.uptc.edu.git.app;
+
+public class Application {
+    private static void emeneses() {
+        System.out.println("Hello Edgar Meneses");
+    }
+
+    public static void andresBarrera(){
+        System.out.println("hello andres barrera");
+        System.out.println("hello andres barrera");
+    }
+
+    public static void main(String[] args) {
+        emeneses();
+        andresBarrera();
+    }
+}
